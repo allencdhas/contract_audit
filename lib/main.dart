ce93635451f9d3443a1f7a9ed8893d7e493c802b
Allen Chrisso Dhas <allencdvja@gmail.com>
2024-06-02 22:32:22 +0530

    color update

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SmartContact Audit App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
+      theme:
+          ThemeData(scaffoldBackgroundColor: Color.fromARGB(255, 61, 58, 156)),
       home: Landing(),
     );
   }
